1ccc09ba4f5dd9d94199f3dae9f2bc5b92a6b0ec
=YanisAF <yanissf2@gmail.com>
2024-06-20 15:50:29 +0200

    first commit

---
+++ exercice1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Chaise une = new Chaise(4, "Bois", "bleu");
+        Chaise deux = new Chaise(3, "bambou", "gris");
+        Chaise trois = new Chaise(1, "métal", "orange");
+        System.out.println(deux);
+    }
+}
