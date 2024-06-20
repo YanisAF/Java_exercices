95e77a71b38690d11cbd745788e4f8832c802820
=YanisAF <yanissf2@gmail.com>
2024-06-20 16:25:26 +0200

    modify1

---
+++ exercice3/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Joueur un = new Joueur("Joueur 1");
+
+        for (int i = 0; i < 11; i++) {
+            un.effectuerUneQuete();
+            System.out.println(un);
+        }
+    }
+}
