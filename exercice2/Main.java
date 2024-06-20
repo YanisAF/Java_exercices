98271f2da4f4af23b76337682b2f8e2c6ba052f5
=YanisAF <yanissf2@gmail.com>
2024-06-20 16:11:45 +0200

    second commit

---
+++ exercice2/Main.java
@@ -0,0 +1,18 @@
+package exercice2;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        org.example.exercice2.Film un = new org.example.exercice2.Film("La Haine", "Matthieu Kassovitz", LocalDate.of(1995,05,31), "Drame");
+        org.example.exercice2.Film deux = new org.example.exercice2.Film("Avatar 2", "James Cameron", LocalDate.of(2022,12,14), "Action");
+
+        System.out.println(deux.toString());
+        System.out.println(un.toString());
+
+        un.afficher();
+
+    }
+
+}
